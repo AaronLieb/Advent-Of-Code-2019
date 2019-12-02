ede9e4db8d8da3bbb5f2df904ec892a7a6f12f14
Aaron <aarnliebrmn@gmail.com>
2019-12-02 15:54:17 -0800

    added part1.js

---
+++ day2/part1.js
@@ -0,0 +1,23 @@
+let input = [];
+
+/*
+What value is left at position 0 after the program halts?
+*/
+
+function run(ic) {
+  for (let i = 0; i < ic.length; i+=4) {
+    let op = ic[i];
+    if (op == 99) return ic;
+    else if (op == 1) {
+      ic[ic[i+3]] = ic[ic[i+1]] + ic[ic[i+2]];
+    } else if (op == 2) {
+      ic[ic[i+3]] = ic[ic[i+1]] * ic[ic[i+2]];
+    } else {
+      console.log("error: ", op);
+      return ic;
+    }
+  }
+  return ic[0];
+}
+
+console.log(run(input))
